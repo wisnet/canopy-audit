382d7a557d754affd9d69f63b08b25d58b54e97a
Michael Dahlke <mdahlke@wisnet.com>
2021-10-17 10:21:05 -0500

    add screenshot and reverse order of reports

---
+++ components/GooglePageSpeedInsights/AuditItems/AuditItemFinalScreenshot.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AuditItemFinalScreenshot"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/GooglePageSpeedInsights/AuditItems/AuditItemFullPageScreenshot.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="audit-item-custom">
+    <v-expansion-panel-header>
+      <v-row cols="12">
+        <v-col cols="9">{{ item.title }} <br />(<small>{{ auditKey }}</small>)</v-col>
+      </v-row>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-img>
+        <img :src="item.details.data"/>
+      </v-img>
+    </v-expansion-panel-content>
+  </section>
+</template>
+
+<script>
+import auditItem from '~/mixins/audit-item';
+
+export default {
+  name: 'AuditItemFinalScreenshot',
+  mixins: [
+    auditItem
+  ]
+};
+</script>
+
+<style scoped>
+
+</style>
